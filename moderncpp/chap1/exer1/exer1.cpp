78ca40c0140e3a95b4c301cb8590a40e61398544
natsutan <km1km1km1>
2021-06-20 17:25:00 +0900

    exer1 終了

--- moderncpp/chap1/exer1/exer1.cpp
+++ moderncpp/chap1/exer1/exer1.cpp
@@ -1,9 +1,25 @@
-#include <cstdio>
+// 与えられた上限までの3または5で割り切れる正の整数の総和を計算して出力するプ
+//ログラムを書きなさい。
+#include <iostream>
+
+unsigned long long func(unsigned int max_v);
+
+unsigned long long func(unsigned int max_v){
+
+    unsigned long long sum = 0;
+    for(auto i=1;i<max_v+1;i++) {
+        if((i%3==0) || (i%5==0)) {
+            sum += i;
+        }
+    }
+    return sum;
+}
 
 int main()
 {
+    auto result = func(10);
 
-    printf("hello\n");
+    std::cout << "result = " << result << std::endl;
 
     return 0;
 }
