60a1ab9aa433b014ebbd4a72a638de2a05e0c619
natsutan <YRW03704@nifty.com>
2021-06-29 21:35:47 +0900

    chap8登録

---
+++ moderncpp/chap1/exer8/exer8.cpp
@@ -0,0 +1,65 @@
+//
+// Created by natu on 2021/06/22.
+//
+// 3桁のアームストロング数を出力する。
+#include <iostream>
+#include <vector>
+#include <numeric>
+#include <unordered_map>
+
+std::vector<int> get_divisors(int x);
+int sum_of_divisors(int x);
+void print_amicable(int x);
+
+std::unordered_map<int, int> memo;
+
+int main(void)
+{
+    memo.clear();
+    print_amicable(1000000);
+
+
+    return 0;
+}
+
+void print_amicable(int x)
+{
+    for(int i=0;i<x;++i) {
+        int sum0 = sum_of_divisors(i);
+        int sum1 = sum_of_divisors(sum0);
+        //std::cout << i << "," << sum0 << "," << sum1 << std::endl;
+
+        if((i == sum1) && (i != sum0) && (sum0 > sum1)) {
+            std::cout << "amicable! " << i << "," << sum0 << std::endl;
+        }
+    }
+
+    std::cout << std::endl;
+}
+
+int sum_of_divisors(int x)
+{
+    if (memo[x] != 0) {
+        return memo[x];
+    }
+    auto divisors = get_divisors(x);
+    int sum = std::accumulate(divisors.begin(), divisors.end(), 0);
+
+    memo[x] = sum;
+
+    return sum;
+}
+
+
+std::vector<int> get_divisors(int x)
+{
+    std::vector<int> divisors;
+
+    for(int i=1;i<x;i++) {
+        if ((x % i) == 0) {
+            divisors.push_back(i);
+        }
+    }
+
+    return divisors;
+}
