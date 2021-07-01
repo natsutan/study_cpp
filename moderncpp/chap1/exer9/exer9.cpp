f7afe0ac5dadfdd8c7136b7bbff3380c518db7b0
natsutan <YRW03704@nifty.com>
2021-07-01 12:42:07 +0900

    exer9動いた

---
+++ moderncpp/chap1/exer9/exer9.cpp
@@ -0,0 +1,40 @@
+//
+// Created by natu on 2021/06/22.
+//
+// 3桁のアームストロング数を出力する。
+#include <iostream>
+#include <sstream>
+#include <cmath>
+#include <vector>
+
+std::vector<int> prime_factorization(int x);
+
+
+int main(void)
+{
+    auto ret = prime_factorization(20);
+
+    for(auto it = ret.begin();it!=ret.end();++it) {
+        std::cout << *it << " ";
+    }
+    std::cout << std::endl;
+
+    return 0;
+}
+
+
+std::vector<int> prime_factorization(int x){
+    std::vector<int> primes;
+
+    int tmp = x;
+    for(auto i=2;i<(x/2);++i) {
+        while((tmp % i) == 0) {
+            primes.push_back(i);
+            tmp = tmp / i;
+        }
+    }
+
+
+    return primes;
+}
+
