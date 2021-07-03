ac02acc8bae001f4313a810e53c29d01ffdd9e82
natsutan <YRW03704@nifty.com>
2021-07-03 22:53:31 +0900

    exer11動いた

---
+++ moderncpp/chap1/exer11/exer11.cpp
@@ -0,0 +1,67 @@
+
+// 11 ローマ数字に変換 与えられた数をローマ数字で表して出力するプログラムを書きなさい。
+// https://gist.github.com/kumo/a8e1cb1f4b7cff1548c7
+#include <iostream>
+#include <vector>
+
+struct roman_pair {
+        int v;
+        std::string s;
+};
+
+
+
+void print_roma(int x);
+std::vector<roman_pair> create_roman_table();
+
+
+int main(void)
+{
+    for(int i=0;i<110;++i) {
+        print_roma(i);
+    }
+
+
+    return 0;
+}
+
+void print_roma(int x)
+{
+    auto roman_table = create_roman_table();
+    std::string s="";
+    auto roman_num = x;
+
+    for(auto it = roman_table.begin();it!=roman_table.end();++it) {
+        auto v = it->v;
+        int div = roman_num / v;
+        if (div > 0) {
+            for(int j=0;j<div;j++) {
+                s = s + it->s;
+            }
+            roman_num -= v * div;
+        }
+    }
+
+    std::cout << s << std::endl;
+}
+
+std::vector<roman_pair> create_roman_table()
+{
+    std::vector<roman_pair> table;
+    table.push_back({1000, "M"});
+    table.push_back({900, "CM"});
+    table.push_back({500, "D"});
+    table.push_back({400, "CD"});
+    table.push_back({100, "C"});
+    table.push_back({90, "XC"});
+    table.push_back({50, "L"});
+    table.push_back({40, "XL"});
+    table.push_back({10, "X"});
+    table.push_back({9, "IX"});
+    table.push_back({5, "V"});
+    table.push_back({4, "IV"});
+    table.push_back({1, "I"});
+
+    return table;
+}
+
