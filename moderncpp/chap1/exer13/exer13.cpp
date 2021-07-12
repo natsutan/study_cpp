d099ba00208bd502c1336dcac76917f413780ac4
natsutan <YRW03704@nifty.com>
2021-07-12 23:13:42 +0900

    exer13終了

---
+++ moderncpp/chap1/exer13/exer13.cpp
@@ -0,0 +1,16 @@
+
+//13 πの計算
+// π（円周率、 PIと 書く ） の値を小数第2位までの精度で計算するプログラムを書き なさい。
+#include <iostream>
+#include <bits/stdc++.h>
+#include <iomanip>
+
+
+int main(void)
+{
+
+    std::cout << std::fixed << std::setprecision(2) <<  M_PI << std::endl;
+
+    return 0;
+}
+
