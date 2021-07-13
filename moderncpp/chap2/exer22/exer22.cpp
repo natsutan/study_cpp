85ad742ce7dad6e03c1d2f3f231df258d79f4921
natsutan <YRW03704@nifty.com>
2021-07-13 21:20:22 +0900

    chap2 スケルトン登録

---
+++ moderncpp/chap2/exer22/exer22.cpp
@@ -0,0 +1,15 @@
+//
+// Created by natu on 2021/06/22.
+//
+
+#include <iostream>
+#include <numeric>
+
+int main(void)
+{
+    auto lcm_ret = std::lcm(80, 60);
+
+    std::cout << lcm_ret << std::endl;
+
+    return 0;
+}
