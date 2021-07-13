3ddc8c2ce798d6790ea7e401769a64674e3be8f2
natsutan <YRW03704@nifty.com>
2021-07-13 21:26:57 +0900

    問題文追加

--- moderncpp/chap2/exer19/exer19.cpp
+++ moderncpp/chap2/exer19/exer19.cpp
@@ -1,6 +1,9 @@
 //
 // Created by natu on 2021/06/22.
 //
+//19 任意個数の要素をコンテナに追加
+//push_back(T&& value)メ ソッドを備えたコンテナの末尾に任意個数の要素を追加で
+//きる汎用の関数を書きなさい。
 
 #include <iostream>
 #include <numeric>
--- moderncpp/chap2/exer20/exer20.cpp
+++ moderncpp/chap2/exer20/exer20.cpp
@@ -1,6 +1,17 @@
 //
 // Created by natu on 2021/06/22.
 //
+//20 コンテナのany, all, none関数
+//        与えられた引数がコンテナの要素のどれか（any）、 すべて（all）、 あるいはどれでも
+//        ない（none） かをチェックする汎用の関数を書きなさい。 次のよう なコードが書けるよう
+//にしなさい＊ 1。
+//std::vector<int> v{ 1, 2, 3, 4, 5, 6 };
+//assert(contains_any(v, 0, 3, 30));
+//std::array<int, 6> a{ { 1, 2, 3, 4, 5, 6 } };
+//assert(contains_all(a, 1, 3, 5, 6));
+//std::list<int> l{ 1, 2, 3, 4, 5, 6 };
+//assert(!contains_none(l, 0, 6));
+
 
 #include <iostream>
 #include <numeric>
