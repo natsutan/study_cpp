ad5f22ef9d18b3773e69d1c650269cfd02d2ac6e
natsutan <km1km1km1>
2021-10-10 19:32:40 +0900

    work

--- moderncpp/chap2/exer17/exer17.cpp
+++ moderncpp/chap2/exer17/exer17.cpp
@@ -8,12 +8,60 @@
 
 #include <iostream>
 #include <numeric>
+#include <vector>
+#include <iostream>
+
+template <typename T> class Con2d {
+public:
+    int width(void) const { return _w;}
+    int height(void) const {return _h;}
+    int size(void) const { return _w * _h;}
+    const T& data(int h, int w) const {
+        return _data[h][w];
+    }
+    T* at(int h, int w) {
+        return &_data[h][w];
+    }
+
+
+    Con2d(int h, int w, T ivalue) {
+        _w = w;
+        _h = h;
+        for(int i=0;i<h;++i) {
+            std::vector<T> elm(w, ivalue);
+            _data.push_back(elm);
+        }
+    }
+
+private:
+    int _w;
+    int _h;
+    std::vector<std::vector<T>> _data;
+
+    //std::ostream& operator<<(std::ostream& os, const Con2d<T>& m);
+};
+
+template <typename T> std::ostream& operator<<(std::ostream& os, const Con2d<T>& m)
+{
+    os << "size " << m.width() << "," << m.height() << std::endl;
+    for(int h=0;h<m.height();++h) {
+        for(int w=0;w<m.width();++w) {
+            os << m.data(h, w);
+            if(w!=(m.width()-1)) {
+                os << ", ";
+            }
+        }
+        os << std::endl;
+    }
+    return os;
+}
 
 int main(void)
 {
-    auto lcm_ret = std::lcm(80, 60);
+    Con2d<int> mat(2, 3, 0);
 
-    std::cout << lcm_ret << std::endl;
+    std::cout << mat;
 
     return 0;
 }
+
